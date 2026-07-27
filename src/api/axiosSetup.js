5e0bd73172d1edf9dcae90fd6a19c96e5f902bfd
pratham <cprathamesh199@gmail.com>
2026-07-27 13:19:40 +0530

    Api Protocol change

--- src/api/axiosSetup.js
+++ src/api/axiosSetup.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://13.235.67.169:8081';
+export const API_BASE_URL = 'https://13.235.67.169';
 
 // Attach stored idToken as Bearer on every outgoing request
 axios.interceptors.request.use(request => {
