271af2d8a0e2088cfd1de58a060794ee23649065
pratham <cprathamesh199@gmail.com>
2026-09-03 14:27:05 +0530

    Axios Changed

--- src/api/axiosSetup.js
+++ src/api/axiosSetup.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { clearAuthStorage } from '../utils/auth.js';
 
-export const API_BASE_URL = 'http://localhost:8081'; // local dev
-// export const API_BASE_URL = 'https://api.cyfenix.com'; // production
+// export const API_BASE_URL = 'http://localhost:8081'; // local dev
+export const API_BASE_URL = 'https://api.cyfenix.com'; // production
 export const GOOGLE_CLIENT_ID = '77085865510-ra9jjmlc59c5ia7eqhs6ler10u266fhf.apps.googleusercontent.com';
 
 
